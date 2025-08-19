5b88b1ff56c0ecedd2ef2fa0cd984872f8080e57
feda <asdost555@gmail.com>
2025-08-18 17:15:31 -0800

    add tailwind

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+<div className='text-red-600 text-[50px] w-[100%]p  mr-auto ml-auto' >hello feda</div>
+    </>
+  )
+}
+
+export default App
